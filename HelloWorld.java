7feed456faaa2f904e3387b1af4c6cba4a40bf88
balakrishna-2806 <107243151+balakrishna-2806@users.noreply.github.com>
2022-06-18 15:16:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("My name is Balakrishna Reddy");
+    System.out.println("My name is Siva");
   }
 }
